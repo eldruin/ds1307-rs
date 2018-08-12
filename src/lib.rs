0212881cb9787fd2712566b1df77dce8b25560df
Diego Barrios Romero <eldruin@gmail.com>
2018-08-12 20:06:21 +0200

    Add more information about the device to the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
 //! This is a platform agnostic Rust driver for the DS1307 real-time clock,
 //! based on the [`embedded-hal`](https://github.com/japaric/embedded-hal) traits.
+//!
+//! ## The device
+//!
+//! The DS1307 serial real-time clock (RTC) is a low-power, full binary-coded
+//! decimal (BCD) clock/calendar plus 56 bytes of NV SRAM. Address and data are
+//! transferred serially through an I2C, bidirectional bus.
+//!
+//! The clock/calendar provides seconds, minutes, hours, day, date, month, and
+//! year information. The end of the month date is automatically adjusted for months
+//! with fewer than 31 days, including corrections for leap year. The clock
+//! operates in either the 24-hour or 12-hour format with AM/PM indicator.
+//!
+//! The DS1307 has a built-in power-sense circuit that detects power failures and
+//! automatically switches to the backup supply.
+//! Timekeeping operation continues while the part operates from the backup supply.
+//!
+//! Datasheet: [DS1307](https://datasheets.maximintegrated.com/en/ds/DS1307.pdf)
 
 #![deny(unsafe_code)]
 #![deny(missing_docs)]
