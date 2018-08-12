ead8e1bd888e8b8e71b3db3a3b7761b74410c388
Diego Barrios Romero <eldruin@gmail.com>
2018-08-12 14:25:52 +0200

    Add function to set the seconds

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,10 @@ use hal::blocking::i2c::{Write, WriteRead};
 pub enum Error<E> {
     /// I²C bus error
     I2C(E),
+    /// Invalid input data.
+    InvalidInputData,
     /// Internal error. Please report this if it ever happens.
-    InternalError
+    InternalError,
 }
 
 /// Hours in either 12-hour (AM/PM) or 24-hour format
@@ -125,6 +127,25 @@ where
             .map_err(Error::I2C)
             .and(Ok(2000 + packed_bcd_to_decimal(data[0]) as u16))
     }
+    
+    /// Set the seconds (0-59)
+    /// Will thrown an InvalidInputData error if the seconds are out of range.
+    pub fn set_seconds(&mut self, seconds: u8) -> Result<(), Error<E>> {
+        if seconds > 59 {
+            return Err(Error::InvalidInputData);
+        }
+        // needs to keep the CH bit status so we read it first
+        let mut data = [0];
+        if let Err(e) = self.i2c
+            .write_read(DEVICE_ADDRESS, &[0x00], &mut data)
+            .map_err(Error::I2C) {
+            return Err(e);
+        }
+        let payload: [u8; 2] = [0, data[0] & 0x80 | decimal_to_packed_bcd(seconds)];
+        self.i2c
+            .write(DEVICE_ADDRESS, &payload)
+            .map_err(Error::I2C)
+    }
 }
 
 fn remove_ch_bit(value: u8) -> u8 {
@@ -135,6 +156,12 @@ fn remove_ch_bit(value: u8) -> u8 {
 fn packed_bcd_to_decimal(bcd: u8) -> u8 {
     (bcd >> 4) * 10 + (bcd & 0xF)
 }
+
+/// Transforms a decimal number to packed BCD format
+fn decimal_to_packed_bcd(dec: u8) -> u8 {
+    ((dec / 10) << 4) | (dec % 10)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -171,6 +198,29 @@ mod tests {
     fn ch_bit_is_ignored() {
         let mut rtc = setup(&[0b1101_1001]);
         assert_eq!(59, rtc.get_seconds().unwrap());
+    }
+
+    #[test]
+    fn wrong_seconds_returns_error() {
+        let mut rtc = setup(&[0]);
+        match rtc.set_seconds(60) {
+            Err(Error::InvalidInputData) => (),
+            _ => panic!(),
+        }
+    }
+    
+    #[test]
+    fn can_write_seconds() {
+        let mut rtc = setup(&[0]);
+        rtc.set_seconds(59).unwrap();
+        check_sent_data(rtc, &[0, 0b0101_1001]);
+    }
+    
+    #[test]
+    fn ch_bit_is_kept_when_writing_seconds() {
+        let mut rtc = setup(&[0b1000_0000]);
+        rtc.set_seconds(59).unwrap();
+        check_sent_data(rtc, &[0, 0b1101_1001]);
     }
 
     #[test]
@@ -249,6 +299,19 @@ mod tests {
         assert_eq!(20, packed_bcd_to_decimal(0b0010_0000));
         assert_eq!(21, packed_bcd_to_decimal(0b0010_0001));
         assert_eq!(59, packed_bcd_to_decimal(0b0101_1001));
+    }
+    
+    #[test]
+    fn can_convert_decimal_to_packed_bcd() {
+        assert_eq!(0b0000_0000, decimal_to_packed_bcd( 0));
+        assert_eq!(0b0000_0001, decimal_to_packed_bcd( 1));
+        assert_eq!(0b0000_1001, decimal_to_packed_bcd( 9));
+        assert_eq!(0b0001_0000, decimal_to_packed_bcd(10));
+        assert_eq!(0b0001_0001, decimal_to_packed_bcd(11));
+        assert_eq!(0b0001_1001, decimal_to_packed_bcd(19));
+        assert_eq!(0b0010_0000, decimal_to_packed_bcd(20));
+        assert_eq!(0b0010_0001, decimal_to_packed_bcd(21));
+        assert_eq!(0b0101_1001, decimal_to_packed_bcd(59));
     }
 }
 
