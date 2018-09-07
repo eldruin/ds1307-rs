14ee0b8f2e6b5531a429fe150515f6c7d7508fb3
Diego Barrios Romero <eldruin@gmail.com>
2018-09-07 18:47:05 +0200

    Simplify method implementation

--- src/run.rs
+++ src/run.rs
@@ -19,26 +19,12 @@ where
     /// Set the clock to run (default on power-on).
     /// (Does not alter the device register if already running).
     pub fn set_running(&mut self) -> Result<(), Error<E>> {
-        // needs to keep the seconds so we read it first
-        let data = self.read_register(Register::SECONDS)?;
-        if (data & BitFlags::CH) == 0 {
-            self.write_register(Register::SECONDS, data | BitFlags::CH)
-        }
-        else {
-            Ok(())
-        }
+        self.set_register_bit_flag(Register::SECONDS, BitFlags::CH)
     }
 
     /// Halt the clock.
     /// (Does not alter the device register if already halted).
     pub fn halt(&mut self) -> Result<(), Error<E>> {
-        // needs to keep the seconds so we read it first
-        let data = self.read_register(Register::SECONDS)?;
-        if (data & BitFlags::CH) != 0 {
-            self.write_register(Register::SECONDS, data & !BitFlags::CH)
-        }
-        else {
-            Ok(())
-        }
+        self.clear_register_bit_flag(Register::SECONDS, BitFlags::CH)
     }
 }
